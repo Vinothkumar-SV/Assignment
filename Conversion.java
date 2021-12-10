e188e8477225d34fb6201bfe98f62fe8a3e219a6
Vinothkumar-SV <95397492+Vinothkumar-SV@users.noreply.github.com>
2021-12-10 13:02:50 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,22 @@
+package javapgm;
+
+public class Conversion {
+
+	public static void main(String[] args) {
+		
+
+
+		int number = -40;
+				
+				if(number < 0) {
+
+				
+		        number = -number;
+					
+					System.out.println("negative -40 is converted to positive "+ number );
+				}
+			
+			}
+		
+	}
+
---
+++ Positive.java
@@ -0,0 +1,30 @@
+package javapgm;
+
+public class Positive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				
+				int number = 35;
+				
+				if(number < 0) {
+
+					System.out.println("The number "+number+" is negative");
+				}
+				else if(number > 0){
+					System.out.println("The number "+number+" is positive");
+					
+				}
+				else {
+					
+					System.out.println("The number "+number+" is neither positive nor negative");
+				}
+			
+		 
+				
+
+		
+	}
+
+}
